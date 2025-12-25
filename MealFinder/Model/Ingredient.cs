8db7c72e604b7c8e13337a1c0bc93274cf3c4afb
taxdevid <arigunatakwa669@gmail.com>
2025-12-25 15:45:24 +0700

    isi class databe dan model, mengisi sedikit code di model class

---
+++ MealFinder/Model/Ingredient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealFinder.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+
+        public bool IsValid()
+        {
+            return !string.IsNullOrWhiteSpace(Name) && Stock >= 0;
+        }
+    }
+}
---
+++ MealFinder/Model/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealFinder.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        // Validasi sederhana (opsional tapi bagus)
+        public bool IsValid()
+        {
+            return !string.IsNullOrWhiteSpace(Name)
+                && !string.IsNullOrWhiteSpace(Username)
+                && !string.IsNullOrWhiteSpace(Email)
+                && !string.IsNullOrWhiteSpace(Password);
+        }
+    }
+}
