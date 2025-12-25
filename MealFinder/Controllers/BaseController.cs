b7c1f0b55e7d397cda154327aaf8b1ab48bf8136
taxdevid <arigunatakwa669@gmail.com>
2025-12-25 15:40:29 +0700

    isi sedikit code dan folder

--- MealFinder/Controllers/BaseController.cs
+++ MealFinder/Controllers/BaseController.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MealFinder.Controller
+using MealFinder.Database;
+using System.Data.SqlClient;
+
+namespace MealFinder.Controllers
 {
-    internal class BaseController
+    public abstract class BaseController
     {
+        protected SqlConnection GetConnection()
+        {
+            return Db.Conn();
+        }
     }
 }
+
