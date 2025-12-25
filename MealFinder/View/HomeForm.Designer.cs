7737c66e576133bd942e2766e66782c7137627f0
DamFran <adamzhafran999@gmail.com>
2025-12-25 18:00:16 +0700

    Update UI HomeForm.

---
+++ MealFinder/View/HomeForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace MealFinder.View
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnRecipe = new FontAwesome.Sharp.IconButton();
+            this.btnHome = new FontAwesome.Sharp.IconButton();
+            this.PanelLogo = new System.Windows.Forms.Panel();
+            this.btnContact = new FontAwesome.Sharp.IconButton();
+            this.panelMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(40)))), ((int)(((byte)(84)))));
+            this.panelMenu.Controls.Add(this.btnContact);
+            this.panelMenu.Controls.Add(this.btnRecipe);
+            this.panelMenu.Controls.Add(this.btnHome);
+            this.panelMenu.Controls.Add(this.PanelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(240, 647);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // btnRecipe
+            // 
+            this.btnRecipe.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnRecipe.FlatAppearance.BorderSize = 0;
+            this.btnRecipe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRecipe.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnRecipe.IconChar = FontAwesome.Sharp.IconChar.BowlFood;
+            this.btnRecipe.IconColor = System.Drawing.Color.Gainsboro;
+            this.btnRecipe.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnRecipe.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRecipe.Location = new System.Drawing.Point(0, 200);
+            this.btnRecipe.Name = "btnRecipe";
+            this.btnRecipe.Padding = new System.Windows.Forms.Padding(10, 10, 20, 0);
+            this.btnRecipe.Size = new System.Drawing.Size(240, 60);
+            this.btnRecipe.TabIndex = 2;
+            this.btnRecipe.Text = "Recipe";
+            this.btnRecipe.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRecipe.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnRecipe.UseVisualStyleBackColor = true;
+            this.btnRecipe.Click += new System.EventHandler(this.btnRecipe_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnHome.IconChar = FontAwesome.Sharp.IconChar.House;
+            this.btnHome.IconColor = System.Drawing.Color.Gainsboro;
+            this.btnHome.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHome.Location = new System.Drawing.Point(0, 140);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnHome.Size = new System.Drawing.Size(240, 60);
+            this.btnHome.TabIndex = 0;
+            this.btnHome.Text = "Home";
+            this.btnHome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHome.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // PanelLogo
+            // 
+            this.PanelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelLogo.Location = new System.Drawing.Point(0, 0);
+            this.PanelLogo.Name = "PanelLogo";
+            this.PanelLogo.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.PanelLogo.Size = new System.Drawing.Size(240, 140);
+            this.PanelLogo.TabIndex = 1;
+            // 
+            // btnContact
+            // 
+            this.btnContact.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnContact.FlatAppearance.BorderSize = 0;
+            this.btnContact.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnContact.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnContact.IconChar = FontAwesome.Sharp.IconChar.Phone;
+            this.btnContact.IconColor = System.Drawing.Color.Gainsboro;
+            this.btnContact.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnContact.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnContact.Location = new System.Drawing.Point(0, 587);
+            this.btnContact.Name = "btnContact";
+            this.btnContact.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.btnContact.Size = new System.Drawing.Size(240, 60);
+            this.btnContact.TabIndex = 3;
+            this.btnContact.Text = "Contact us";
+            this.btnContact.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnContact.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnContact.UseVisualStyleBackColor = true;
+            this.btnContact.Click += new System.EventHandler(this.btnContact_Click);
+            // 
+            // HomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 647);
+            this.Controls.Add(this.panelMenu);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "HomeForm";
+            this.Text = "HomeForm";
+            this.panelMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private FontAwesome.Sharp.IconButton btnHome;
+        private System.Windows.Forms.Panel PanelLogo;
+        private FontAwesome.Sharp.IconButton btnRecipe;
+        private FontAwesome.Sharp.IconButton btnContact;
+    }
+}
---
+++ MealFinder/View/HomeForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FontAwesome.Sharp;
+
+namespace MealFinder.View
+{
+    public partial class HomeForm : Form
+    {
+        private IconButton currentButton;
+        private Panel leftBorderBtn;
+            
+
+
+        public HomeForm()
+        {
+            InitializeComponent();
+            leftBorderBtn = new Panel();
+            leftBorderBtn.Size = new Size(10, 60);
+            panelMenu.Controls.Add(leftBorderBtn);
+
+        }
+
+        private struct RGBColors
+        {
+
+            public static Color color1 = Color.FromArgb(17, 126, 241);
+            public static Color color2 = Color.FromArgb(249, 118, 176);
+            public static Color color3 = Color.FromArgb(253, 138, 114);
+
+
+
+
+        }
+
+        private void ActivateButton(object senderBtn, Color color) {
+
+            if (senderBtn != null) { 
+            
+                DisableButton();
+            currentButton=(IconButton)senderBtn;
+                currentButton.BackColor = Color.FromArgb(15, 40, 84);
+                currentButton.ForeColor = color;
+                currentButton.TextAlign = ContentAlignment.MiddleCenter;
+                currentButton.TextImageRelation = TextImageRelation.TextBeforeImage;
+                currentButton.ImageAlign = ContentAlignment.MiddleRight;
+
+                //left border 
+
+                leftBorderBtn.BackColor = color;
+                leftBorderBtn.Location = new Point(0,currentButton.Location.Y);
+                leftBorderBtn.Visible = true;
+                leftBorderBtn.BringToFront();
+            }
+
+        }
+
+        private void DisableButton()
+        {
+
+            if (currentButton != null) {
+
+                currentButton.BackColor = Color.FromArgb(20, 35, 89);
+                currentButton.ForeColor = Color.Gainsboro;
+                currentButton.TextAlign= ContentAlignment.MiddleLeft;
+                currentButton.IconColor = Color.Gainsboro;
+                currentButton.TextImageRelation = TextImageRelation.ImageBeforeText;
+                currentButton.ImageAlign= ContentAlignment.MiddleLeft;
+
+                
+
+            }
+
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color1);
+        }
+
+        private void btnRecipe_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color2);
+        }
+
+        private void btnContact_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color3);
+        }
+    }
+}
