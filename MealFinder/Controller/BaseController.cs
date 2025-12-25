ad786ced4b767f762c34cc1772481b769cbdd56a
DamFran <adamzhafran999@gmail.com>
2025-12-25 15:27:11 +0700

    Add Controller Folder

---
+++ MealFinder/Controller/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealFinder.Controller
+{
+    internal class BaseController
+    {
+    }
+}
