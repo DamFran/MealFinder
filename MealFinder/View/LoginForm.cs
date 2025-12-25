3f576d320693316dadab3d644fb89f583979b9a9
DamFran <adamzhafran999@gmail.com>
2025-12-25 14:37:08 +0700

    Add project files.

---
+++ MealFinder/View/LoginForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MealFinder.View
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = DbConnection.GetConnection())
+            {
+                conn.Open();
+
+                string query = "SELECT COUNT(*) FROM users WHERE username=@user AND password=@pass";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@user", txtUsername.Text);
+                cmd.Parameters.AddWithValue("@pass", txtPassword.Text);
+
+                int result = (int)cmd.ExecuteScalar();
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Login berhasil!");
+                    new HomeForm().Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Username atau Password salah!");
+                }
+            }
+        }
+    }
+}
