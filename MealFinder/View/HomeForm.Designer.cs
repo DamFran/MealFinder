dfb3abb7945efa0be5f0e406de71d9b3236ef7bc
DamFran <adamzhafran999@gmail.com>
2025-12-28 13:07:40 +0700

    View Home sdh bisa di buka ler aman azza

--- MealFinder/View/HomeForm.Designer.cs
+++ MealFinder/View/HomeForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MealFinder.View
 {
-    partial class HomeForm
+    partial class Home
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeForm));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
             this.panelMenu = new System.Windows.Forms.Panel();
-            this.PanelLogo = new System.Windows.Forms.Panel();
             this.btnContact = new FontAwesome.Sharp.IconButton();
             this.btnRecipe = new FontAwesome.Sharp.IconButton();
             this.btnHome = new FontAwesome.Sharp.IconButton();
+            this.PanelLogo = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TitleBar = new System.Windows.Forms.Panel();
+            this.btnRegister = new FontAwesome.Sharp.IconButton();
+            this.btnLogin = new FontAwesome.Sharp.IconButton();
+            this.TitleChildForm = new System.Windows.Forms.Label();
+            this.iconCurrentChildForm = new FontAwesome.Sharp.IconPictureBox();
+            this.panelShadow = new System.Windows.Forms.Panel();
+            this.panelDesktop = new System.Windows.Forms.Panel();
             this.panelMenu.SuspendLayout();
             this.PanelLogo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.TitleBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconCurrentChildForm)).BeginInit();
             this.SuspendLayout();
             // 
             // panelMenu
             // 
-            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(40)))), ((int)(((byte)(84)))));
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(111)))), ((int)(((byte)(43)))));
             this.panelMenu.Controls.Add(this.btnContact);
             this.panelMenu.Controls.Add(this.btnRecipe);
             this.panelMenu.Controls.Add(this.btnHome);
@@ -52,16 +61,6 @@
             this.panelMenu.Name = "panelMenu";
             this.panelMenu.Size = new System.Drawing.Size(240, 647);
             this.panelMenu.TabIndex = 0;
-            // 
-            // PanelLogo
-            // 
-            this.PanelLogo.Controls.Add(this.pictureBox1);
-            this.PanelLogo.Dock = System.Windows.Forms.DockStyle.Top;
-            this.PanelLogo.Location = new System.Drawing.Point(0, 0);
-            this.PanelLogo.Name = "PanelLogo";
-            this.PanelLogo.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
-            this.PanelLogo.Size = new System.Drawing.Size(240, 140);
-            this.PanelLogo.TabIndex = 1;
             // 
             // btnContact
             // 
@@ -126,6 +125,16 @@
             this.btnHome.UseVisualStyleBackColor = true;
             this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
             // 
+            // PanelLogo
+            // 
+            this.PanelLogo.Controls.Add(this.pictureBox1);
+            this.PanelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelLogo.Location = new System.Drawing.Point(0, 0);
+            this.PanelLogo.Name = "PanelLogo";
+            this.PanelLogo.Padding = new System.Windows.Forms.Padding(10, 0, 20, 0);
+            this.PanelLogo.Size = new System.Drawing.Size(240, 140);
+            this.PanelLogo.TabIndex = 1;
+            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::MealFinder.Properties.Resources.MealFinder3;
@@ -137,20 +146,119 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // HomeForm
+            // TitleBar
+            // 
+            this.TitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(128)))), ((int)(((byte)(43)))));
+            this.TitleBar.Controls.Add(this.btnRegister);
+            this.TitleBar.Controls.Add(this.btnLogin);
+            this.TitleBar.Controls.Add(this.TitleChildForm);
+            this.TitleBar.Controls.Add(this.iconCurrentChildForm);
+            this.TitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TitleBar.Location = new System.Drawing.Point(240, 0);
+            this.TitleBar.Name = "TitleBar";
+            this.TitleBar.Size = new System.Drawing.Size(860, 75);
+            this.TitleBar.TabIndex = 1;
+            this.TitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TitleBar_MouseDown);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnRegister.FlatAppearance.BorderSize = 0;
+            this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegister.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnRegister.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.btnRegister.IconColor = System.Drawing.Color.Gainsboro;
+            this.btnRegister.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnRegister.Location = new System.Drawing.Point(540, 0);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Padding = new System.Windows.Forms.Padding(0, 10, 30, 0);
+            this.btnRegister.Size = new System.Drawing.Size(170, 75);
+            this.btnRegister.TabIndex = 4;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnLogin.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.btnLogin.IconColor = System.Drawing.Color.Gainsboro;
+            this.btnLogin.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnLogin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogin.Location = new System.Drawing.Point(710, 0);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Padding = new System.Windows.Forms.Padding(0, 10, 30, 0);
+            this.btnLogin.Size = new System.Drawing.Size(150, 75);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogin.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // TitleChildForm
+            // 
+            this.TitleChildForm.AutoSize = true;
+            this.TitleChildForm.ForeColor = System.Drawing.Color.White;
+            this.TitleChildForm.Location = new System.Drawing.Point(54, 26);
+            this.TitleChildForm.Name = "TitleChildForm";
+            this.TitleChildForm.Size = new System.Drawing.Size(61, 23);
+            this.TitleChildForm.TabIndex = 2;
+            this.TitleChildForm.Text = "Home";
+            // 
+            // iconCurrentChildForm
+            // 
+            this.iconCurrentChildForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(128)))), ((int)(((byte)(43)))));
+            this.iconCurrentChildForm.ForeColor = System.Drawing.Color.Gainsboro;
+            this.iconCurrentChildForm.IconChar = FontAwesome.Sharp.IconChar.House;
+            this.iconCurrentChildForm.IconColor = System.Drawing.Color.Gainsboro;
+            this.iconCurrentChildForm.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconCurrentChildForm.Location = new System.Drawing.Point(16, 21);
+            this.iconCurrentChildForm.Name = "iconCurrentChildForm";
+            this.iconCurrentChildForm.Size = new System.Drawing.Size(32, 32);
+            this.iconCurrentChildForm.TabIndex = 0;
+            this.iconCurrentChildForm.TabStop = false;
+            // 
+            // panelShadow
+            // 
+            this.panelShadow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(109)))), ((int)(((byte)(37)))));
+            this.panelShadow.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelShadow.Location = new System.Drawing.Point(240, 75);
+            this.panelShadow.Name = "panelShadow";
+            this.panelShadow.Size = new System.Drawing.Size(860, 9);
+            this.panelShadow.TabIndex = 2;
+            // 
+            // panelDesktop
+            // 
+            this.panelDesktop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(201)))), ((int)(((byte)(127)))));
+            this.panelDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDesktop.Location = new System.Drawing.Point(240, 84);
+            this.panelDesktop.Name = "panelDesktop";
+            this.panelDesktop.Size = new System.Drawing.Size(860, 563);
+            this.panelDesktop.TabIndex = 3;
+            // 
+            // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1100, 647);
+            this.Controls.Add(this.panelDesktop);
+            this.Controls.Add(this.panelShadow);
+            this.Controls.Add(this.TitleBar);
             this.Controls.Add(this.panelMenu);
             this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
-            this.Name = "HomeForm";
+            this.Name = "Home";
             this.Text = "MealFinder";
             this.panelMenu.ResumeLayout(false);
             this.PanelLogo.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.TitleBar.ResumeLayout(false);
+            this.TitleBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconCurrentChildForm)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -163,5 +271,12 @@
         private FontAwesome.Sharp.IconButton btnRecipe;
         private FontAwesome.Sharp.IconButton btnContact;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel TitleBar;
+        private FontAwesome.Sharp.IconPictureBox iconCurrentChildForm;
+        private System.Windows.Forms.Label TitleChildForm;
+        private System.Windows.Forms.Panel panelShadow;
+        private System.Windows.Forms.Panel panelDesktop;
+        private FontAwesome.Sharp.IconButton btnRegister;
+        private FontAwesome.Sharp.IconButton btnLogin;
     }
 }
--- MealFinder/View/HomeForm.cs
+++ MealFinder/View/HomeForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Drawing.Text;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,20 +13,23 @@ using FontAwesome.Sharp;
 
 namespace MealFinder.View
 {
-    public partial class HomeForm : Form
+    public partial class Home : Form
     {
         private IconButton currentButton;
         private Panel leftBorderBtn;
-            
+        private Form currentChildForm;
 
 
-        public HomeForm()
+        public Home()
         {
             InitializeComponent();
+
             leftBorderBtn = new Panel();
             leftBorderBtn.Size = new Size(10, 60);
-            panelMenu.Controls.Add(leftBorderBtn);
+            leftBorderBtn.BackColor = Color.Transparent;
+            leftBorderBtn.Visible = false;
 
+            panelMenu.Controls.Add(leftBorderBtn);
         }
 
         private struct RGBColors
@@ -34,6 +38,8 @@ namespace MealFinder.View
             public static Color color1 = Color.FromArgb(17, 126, 241);
             public static Color color2 = Color.FromArgb(249, 118, 176);
             public static Color color3 = Color.FromArgb(253, 138, 114);
+
+            public static Color color4 = Color.FromArgb(255, 255, 255);
 
 
 
@@ -46,7 +52,7 @@ namespace MealFinder.View
             
                 DisableButton();
             currentButton=(IconButton)senderBtn;
-                currentButton.BackColor = Color.FromArgb(15, 40, 84);
+                currentButton.BackColor = Color.FromArgb(92, 111, 43);
                 currentButton.ForeColor = color;
                 currentButton.TextAlign = ContentAlignment.MiddleCenter;
                 currentButton.TextImageRelation = TextImageRelation.TextBeforeImage;
@@ -58,6 +64,10 @@ namespace MealFinder.View
                 leftBorderBtn.Location = new Point(0,currentButton.Location.Y);
                 leftBorderBtn.Visible = true;
                 leftBorderBtn.BringToFront();
+
+                //Icon Current Child Form
+                iconCurrentChildForm.IconChar = currentButton.IconChar;
+                iconCurrentChildForm.IconColor = color; 
             }
 
         }
@@ -67,7 +77,7 @@ namespace MealFinder.View
 
             if (currentButton != null) {
 
-                currentButton.BackColor = Color.FromArgb(20, 35, 89);
+                currentButton.BackColor = Color.FromArgb(92, 111, 43);
                 currentButton.ForeColor = Color.Gainsboro;
                 currentButton.TextAlign= ContentAlignment.MiddleLeft;
                 currentButton.IconColor = Color.Gainsboro;
@@ -80,24 +90,72 @@ namespace MealFinder.View
 
         }
 
+        private void OpenChildForm(Form childForm)
+        {
+            if (currentChildForm != null)
+            {
+                currentChildForm.Close();
+            }
+
+            currentChildForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+
+            panelDesktop.Controls.Clear(); // opsional tapi disarankan
+            panelDesktop.Controls.Add(childForm);
+            panelDesktop.Tag = childForm;
+
+            childForm.BringToFront();
+            childForm.Show();
+
+            TitleChildForm.Text = childForm.Name;
+        }
+
         private void btnHome_Click(object sender, EventArgs e)
         {
-            ActivateButton(sender, RGBColors.color1);
+            ActivateButton(sender, RGBColors.color4);
+            
         }
 
         private void btnRecipe_Click(object sender, EventArgs e)
         {
-            ActivateButton(sender, RGBColors.color2);
+            ActivateButton(sender, RGBColors.color4);
+            OpenChildForm(new Recipe());
         }
 
         private void btnContact_Click(object sender, EventArgs e)
         {
-            ActivateButton(sender, RGBColors.color3);
+            ActivateButton(sender, RGBColors.color4);
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
+            Reset();
 
         }
+
+        private void Reset()
+        {
+            DisableButton();
+            leftBorderBtn.Visible = false;
+           
+
+        }
+
+        //Drag Form
+        [DllImport("user32.dll", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.dll", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+
+        private void TitleBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+       
     }
 }
