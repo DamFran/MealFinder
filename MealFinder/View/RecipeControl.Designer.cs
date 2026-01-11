4ab41a015f8de99a602a04fefc2aae0eb9036b92
taxdevid <arigunatakwa669@gmail.com>
2026-01-11 18:05:51 +0700

    feat(recipe): implement recipe list with DataGridView and detail view
    - Add Recipe model for binding recipe data
    - Display available recipes using dgvRecipes
    - Enable row selection to show recipe details
    - Bind recipe data using object-based DataSource
    - Improve UX with full-row selection and read-only grid

--- MealFinder/View/RecipeControl.Designer.cs
+++ MealFinder/View/RecipeControl.Designer.cs
@@ -32,24 +32,24 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
+            this.dgvIngredients = new Guna.UI2.WinForms.Guna2DataGridView();
             this.btnAddIngredient = new Guna.UI2.WinForms.Guna2Button();
             this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2GradientPanel2 = new Guna.UI2.WinForms.Guna2GradientPanel();
+            this.dgvRecipes = new System.Windows.Forms.DataGridView();
             this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2GradientPanel3 = new Guna.UI2.WinForms.Guna2GradientPanel();
-            this.guna2GradientPanel4 = new Guna.UI2.WinForms.Guna2GradientPanel();
-            this.lblRecipeName = new Guna.UI2.WinForms.Guna2HtmlLabel();
-            this.dgvRecipes = new System.Windows.Forms.DataGridView();
             this.picRecipe = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2GradientPanel4 = new Guna.UI2.WinForms.Guna2GradientPanel();
             this.txtRecipeDetail = new Guna.UI2.WinForms.Guna2TextBox();
-            this.dgvIngredients = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.lblRecipeName = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2GradientPanel1.SuspendLayout();
-            this.guna2GradientPanel2.SuspendLayout();
-            this.guna2GradientPanel3.SuspendLayout();
-            this.guna2GradientPanel4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvRecipes)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picRecipe)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvIngredients)).BeginInit();
+            this.guna2GradientPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRecipes)).BeginInit();
+            this.guna2GradientPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picRecipe)).BeginInit();
+            this.guna2GradientPanel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // guna2GradientPanel1
@@ -62,6 +62,57 @@
             this.guna2GradientPanel1.Name = "guna2GradientPanel1";
             this.guna2GradientPanel1.Size = new System.Drawing.Size(300, 570);
             this.guna2GradientPanel1.TabIndex = 0;
+            // 
+            // dgvIngredients
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.dgvIngredients.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvIngredients.BackgroundColor = System.Drawing.Color.Gainsboro;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvIngredients.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvIngredients.ColumnHeadersHeight = 15;
+            this.dgvIngredients.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvIngredients.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvIngredients.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dgvIngredients.Location = new System.Drawing.Point(3, 36);
+            this.dgvIngredients.Name = "dgvIngredients";
+            this.dgvIngredients.RowHeadersVisible = false;
+            this.dgvIngredients.Size = new System.Drawing.Size(294, 495);
+            this.dgvIngredients.TabIndex = 2;
+            this.dgvIngredients.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.dgvIngredients.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.dgvIngredients.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.dgvIngredients.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.dgvIngredients.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.dgvIngredients.ThemeStyle.BackColor = System.Drawing.Color.Gainsboro;
+            this.dgvIngredients.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dgvIngredients.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.dgvIngredients.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dgvIngredients.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgvIngredients.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.dgvIngredients.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.dgvIngredients.ThemeStyle.HeaderStyle.Height = 15;
+            this.dgvIngredients.ThemeStyle.ReadOnly = false;
+            this.dgvIngredients.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.dgvIngredients.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dgvIngredients.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgvIngredients.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.dgvIngredients.ThemeStyle.RowsStyle.Height = 22;
+            this.dgvIngredients.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.dgvIngredients.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
             // 
             // btnAddIngredient
             // 
@@ -101,6 +152,15 @@
             this.guna2GradientPanel2.Size = new System.Drawing.Size(300, 570);
             this.guna2GradientPanel2.TabIndex = 1;
             // 
+            // dgvRecipes
+            // 
+            this.dgvRecipes.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.dgvRecipes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRecipes.Location = new System.Drawing.Point(3, 36);
+            this.dgvRecipes.Name = "dgvRecipes";
+            this.dgvRecipes.Size = new System.Drawing.Size(294, 531);
+            this.dgvRecipes.TabIndex = 1;
+            // 
             // guna2HtmlLabel2
             // 
             this.guna2HtmlLabel2.AutoSize = false;
@@ -123,38 +183,6 @@
             this.guna2GradientPanel3.Size = new System.Drawing.Size(300, 267);
             this.guna2GradientPanel3.TabIndex = 2;
             // 
-            // guna2GradientPanel4
-            // 
-            this.guna2GradientPanel4.BackColor = System.Drawing.Color.Gainsboro;
-            this.guna2GradientPanel4.Controls.Add(this.txtRecipeDetail);
-            this.guna2GradientPanel4.Controls.Add(this.lblRecipeName);
-            this.guna2GradientPanel4.Location = new System.Drawing.Point(633, 289);
-            this.guna2GradientPanel4.Name = "guna2GradientPanel4";
-            this.guna2GradientPanel4.Size = new System.Drawing.Size(300, 297);
-            this.guna2GradientPanel4.TabIndex = 3;
-            // 
-            // lblRecipeName
-            // 
-            this.lblRecipeName.AutoSize = false;
-            this.lblRecipeName.BackColor = System.Drawing.Color.Silver;
-            this.lblRecipeName.Dock = System.Windows.Forms.DockStyle.Top;
-            this.lblRecipeName.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRecipeName.Location = new System.Drawing.Point(0, 0);
-            this.lblRecipeName.Name = "lblRecipeName";
-            this.lblRecipeName.Size = new System.Drawing.Size(300, 30);
-            this.lblRecipeName.TabIndex = 0;
-            this.lblRecipeName.Text = "Detail Resep";
-            this.lblRecipeName.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // dgvRecipes
-            // 
-            this.dgvRecipes.BackgroundColor = System.Drawing.Color.Gainsboro;
-            this.dgvRecipes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvRecipes.Location = new System.Drawing.Point(3, 36);
-            this.dgvRecipes.Name = "dgvRecipes";
-            this.dgvRecipes.Size = new System.Drawing.Size(294, 531);
-            this.dgvRecipes.TabIndex = 1;
-            // 
             // picRecipe
             // 
             this.picRecipe.FillColor = System.Drawing.Color.Gainsboro;
@@ -165,6 +193,16 @@
             this.picRecipe.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picRecipe.TabIndex = 0;
             this.picRecipe.TabStop = false;
+            // 
+            // guna2GradientPanel4
+            // 
+            this.guna2GradientPanel4.BackColor = System.Drawing.Color.Gainsboro;
+            this.guna2GradientPanel4.Controls.Add(this.txtRecipeDetail);
+            this.guna2GradientPanel4.Controls.Add(this.lblRecipeName);
+            this.guna2GradientPanel4.Location = new System.Drawing.Point(633, 289);
+            this.guna2GradientPanel4.Name = "guna2GradientPanel4";
+            this.guna2GradientPanel4.Size = new System.Drawing.Size(300, 297);
+            this.guna2GradientPanel4.TabIndex = 3;
             // 
             // txtRecipeDetail
             // 
@@ -187,55 +225,18 @@
             this.txtRecipeDetail.Size = new System.Drawing.Size(294, 258);
             this.txtRecipeDetail.TabIndex = 1;
             // 
-            // dgvIngredients
+            // lblRecipeName
             // 
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
-            this.dgvIngredients.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            this.dgvIngredients.BackgroundColor = System.Drawing.Color.Gainsboro;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvIngredients.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
-            this.dgvIngredients.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvIngredients.DefaultCellStyle = dataGridViewCellStyle3;
-            this.dgvIngredients.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            this.dgvIngredients.Location = new System.Drawing.Point(3, 36);
-            this.dgvIngredients.Name = "dgvIngredients";
-            this.dgvIngredients.RowHeadersVisible = false;
-            this.dgvIngredients.Size = new System.Drawing.Size(294, 495);
-            this.dgvIngredients.TabIndex = 2;
-            this.dgvIngredients.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
-            this.dgvIngredients.ThemeStyle.AlternatingRowsStyle.Font = null;
-            this.dgvIngredients.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
-            this.dgvIngredients.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
-            this.dgvIngredients.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
-            this.dgvIngredients.ThemeStyle.BackColor = System.Drawing.Color.Gainsboro;
-            this.dgvIngredients.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            this.dgvIngredients.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            this.dgvIngredients.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            this.dgvIngredients.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dgvIngredients.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
-            this.dgvIngredients.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvIngredients.ThemeStyle.HeaderStyle.Height = 15;
-            this.dgvIngredients.ThemeStyle.ReadOnly = false;
-            this.dgvIngredients.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
-            this.dgvIngredients.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
-            this.dgvIngredients.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dgvIngredients.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            this.dgvIngredients.ThemeStyle.RowsStyle.Height = 22;
-            this.dgvIngredients.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            this.dgvIngredients.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.lblRecipeName.AutoSize = false;
+            this.lblRecipeName.BackColor = System.Drawing.Color.Silver;
+            this.lblRecipeName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblRecipeName.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecipeName.Location = new System.Drawing.Point(0, 0);
+            this.lblRecipeName.Name = "lblRecipeName";
+            this.lblRecipeName.Size = new System.Drawing.Size(300, 30);
+            this.lblRecipeName.TabIndex = 0;
+            this.lblRecipeName.Text = "Detail Resep";
+            this.lblRecipeName.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // RecipeControl
             // 
@@ -248,12 +249,12 @@
             this.Name = "RecipeControl";
             this.Size = new System.Drawing.Size(950, 603);
             this.guna2GradientPanel1.ResumeLayout(false);
-            this.guna2GradientPanel2.ResumeLayout(false);
-            this.guna2GradientPanel3.ResumeLayout(false);
-            this.guna2GradientPanel4.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvRecipes)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picRecipe)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvIngredients)).EndInit();
+            this.guna2GradientPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRecipes)).EndInit();
+            this.guna2GradientPanel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picRecipe)).EndInit();
+            this.guna2GradientPanel4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
